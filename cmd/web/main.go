53280a9d5dc925bbe2a970e7d5f9bfc9d52de56e
mgxnch <mgxnch@gmail.com>
2025-07-13 22:47:43 +0800

    feat: add static assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,8 +6,18 @@ import (
 )
 
 func main() {
-	// Initialise mux and declare the routes
+	// Initialise router
 	mux := http.NewServeMux()
+
+	// File server and its route
+	// http.FileServer serves file out of its input directory
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Strip away /static from the incoming request e.g. GET localhost:4000/static/img/logo.png
+	// becomes "img/logo.png" which is a valid path to the fileServer
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Application routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
