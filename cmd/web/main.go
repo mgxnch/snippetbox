89184af789196cd745b25e10442bb8f152d8c38e
mgxnch <mgxnch@gmail.com>
2025-07-14 22:01:20 +0800

    feat: use custom errorLog instead of standard logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// Handle environment config values
 	addr := flag.String("addr", ":4000", "HTTP port")
 	flag.Parse()
+
+	// Initialise loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime)
 
 	// Initialise router
 	mux := http.NewServeMux()
@@ -27,7 +32,13 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on http://localhost:%s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Create HTTP server struct
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("Starting server on http://localhost:%s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
