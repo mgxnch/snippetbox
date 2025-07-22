5bd0020cfffa560b48f2f80136044c3718755e6b
mgxnch <mgxnch@gmail.com>
2025-07-22 22:46:17 +0800

    refactor: use go-chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/justinas/alice"
+	"github.com/go-chi/chi/v5"
 )
 
 // routes sets up a ServeMux and its routes and returns the object.
 func (app *application) routes() http.Handler {
-	// Initialise router
-	mux := http.NewServeMux()
+	// Initialise Chi router
+	r := chi.NewRouter()
+
+	// Middleware chain:
+	// recoverPanic ->logRequest -> secureHeaders -> serverMux -> application handlers
+	r.Use(app.recoverPanic)
+	r.Use(app.logRequest)
+	r.Use(secureHeaders)
 
 	// File server and its route
 	// http.FileServer serves file out of its input directory
@@ -18,19 +24,14 @@ func (app *application) routes() http.Handler {
 
 	// Strip away /static from the incoming request e.g. GET localhost:4000/static/img/logo.png
 	// becomes "img/logo.png" which is a valid path to the fileServer
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	r.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	// Application routes
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	r.HandleFunc("/", app.home)
+	r.HandleFunc("/snippet/view", app.snippetView)
+	r.HandleFunc("/snippet/create", app.snippetCreate)
 
-	// Middleware chain: recoverPanic ->logRequest -> secureHeaders -> serverMux -> application handlers
-
-	// Create a 'standard' middleware chain which contains the middlewares
-	// that will be used for every request our application receives
-	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	return standard.Then(mux)
+	return r
 }
 
 func (app *application) logRequest(next http.Handler) http.Handler {
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/mgxnch/snippetbox
 
 go 1.24.1
 
-require github.com/go-sql-driver/mysql v1.9.3
-
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-sql-driver/mysql v1.9.3
 )
+
+require filippo.io/edwards25519 v1.1.0 // indirect
